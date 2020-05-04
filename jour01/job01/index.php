ba1c9bf24af5f4c5885c2fd5a4a725996432541c
paul-le <paulloukyle@gmail.com>
2020-05-04 10:44:27 +0200

    job01 finis

---
+++ jour01/job01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <link rel="stylesheet" media="screen and (min-width: 1680px) and (max-width: 1920px)" type="text/css" href="style2.css">
+    <link rel="stylesheet" media="screen and (min-width: 1280px) and (max-width: 1680px)" type="text/css" href="style3.css">
+    <link rel="stylesheet" media="screen and (max-width: 1280px)" type="text/css" href="style4.css">
+    <title>Runtrack</title>
+</head>
