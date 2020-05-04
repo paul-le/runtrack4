2bee551f67260f969f1bceb974753ff4a4c9ba67
paul-le <paulloukyle@gmail.com>
2020-05-04 17:09:00 +0200

    J'arrive pas à faire réapparaître le menu après avoir cliquer sur le logo.

---
+++ jour01/job04/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Runtrack</title>
+</head>
+<body>
+    <header>
+        <div id="hamButt"><a href="#toast"><img src="Images/Hamburger_icon.svg.png"></a>
+        </div>
+        <nav id="toast">
+            <ul>
+                <li class="menu">Accueil</li>
+                <li class="menu">Présentation</li>
+                <li class="menu">Contact</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
