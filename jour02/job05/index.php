f885b4f4781b01ddf535a78ae72a93766e6a9b0a
paul-le <paulloukyle@gmail.com>
2020-05-05 20:10:25 +0200

    Push du job5

---
+++ jour02/job05/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="materialize/css/materialize.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Runtrack</title>
+</head>
+<body>
+   <header>
+        <nav class="indigo darken-4 nav-wrapper">
+            <ul nav-mobile class="left hide-on-med-and-down">
+                <img class="brand-logo right" id="nav-mobile" src="Images/output-onlinepngtools.png">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="index.php">Inscription</a></li>
+                <li><a href="index.php">Connexion</a></li>
+                <li><a href="index.php">Rechercher</a></li>
+            </ul>
+        </nav>
+   </header>
+   <main class="container">
+   <div class="column">
+   <div class="col s12 m7">
+            <div class="card medium">
+                <div class="card-image">
+                <img src="Images/image5.jpg">
+                <span class="card-title">Random girl</span>
+                </div>
+                <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                <a href="https://kuvshinov_ilya.artstation.com/projects/RYA3wX">Sauce</a>
+                </div>
+            </div>
+        </div>
+    <div class="row">
+        <div class="col s12 m7">
+            <div class="card medium">
+                <div class="card-image">
+                <img src="Images/image2.jpg">
+                <span class="card-title">Random dude</span>
+                </div>
+                <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                <a href="https://kuvshinov_ilya.artstation.com/projects/RYA3wX">Sauce</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col s12 m7">
+            <div class="card horizontal">
+                <div class="card-image">
+                <img src="Images/image1.jpg">
+                <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                <a href="https://kuvshinov_ilya.artstation.com/projects/RYA3wX">Sauce</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m7">
+            <div class="card horizontal">
+                <div class="card-image">
+                <img src="Images/image3.jpg">
+                <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                <a href="https://kuvshinov_ilya.artstation.com/projects/RYA3wX">Sauce</a>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="col s12 m7">
+            <div class="card horizontal">
+                <div class="card-image">
+                <img src="Images/image4.jpg">
+                <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                <a href="https://kuvshinov_ilya.artstation.com/projects/RYA3wX">Sauce</a>
+                </div>
+            </div>
+        </div>
+    </div>
+   </main>
+   <footer class="indigo darken-3 page-footer">
+        <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer blabla.</h5>
+                <p class="grey-text text-lighten-4">Blablabla.</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Links</h5>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="index.php">Inscription</a></li>
+                <li><a href="index.php">Connexion</a></li>
+                <li><a href="index.php">Rechercher</a></li>
+            </ul>
+            </div>
+            </div>
+          </div>
+          <div class="indigo darken-4 footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </div>
+   </footer> 
+</body>
+</html>
