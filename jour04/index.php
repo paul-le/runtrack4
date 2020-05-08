e1d78c23abf5c4e74dee51198762595f6c7dfb25
paul-le <paulloukyle@gmail.com>
2020-05-08 16:09:56 +0200

    Avancement responsive mobile de l'index , je peux opti le html mais la flemme.

--- jour04/index.php
+++ jour04/index.php
@@ -51,8 +51,11 @@
                     </div>
                 </div>
             </section>
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
             <section id="cards-top-mobile">
-                <div id="card-index-own-style" class="card" style="width: 35rem;">
+                <div id="card-index-own-style" class="card" style="width: 55rem;">
                     <img src="assets/toast2.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Vous en voulez au confinement , le travaille de groupe vous manque ? Ne vous en faites pas ! Ce site sera là pour avoir accés aux locos pour enfin retrouver vos collègues et travailler sérieusement ! (oui oui oui)</p>
@@ -60,29 +63,43 @@
                 </div>
                 <br>
                 <br>
-                <div id="card-index-own-style2" class="card" style="width: 35rem;">
+                <div id="card-index-own-style2" class="card" style="width: 55rem;">
                     <img src="assets/WorkingTogether.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Car oui , le travaille en groupe est important dans cette formation où l'entraide prône ! Travailler dans son coin est contre-productif !</p>
                     </div>
                 </div>
             </section>
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
             <section id="section-index-news">
                 <div id="card-index-own-style3" class="card" style="width: 65rem;">
                     <img src="assets/news.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Blabla : Le Premier ministre Edouard Philippe a rendez-vous à 16 heures, ce jeudi 7 mai, pour présenter depuis Matignon "la préparation de l'étape du 11 mai" concoctée par son gouvernement, ainsi que la carte du déconfinement par département. Son discours est à suivre sur cette page, en direct et en vidéo.<br>
-                        Le plan de sortie du confinement devrait alors être complet, au moins pour sa première phase qui devrait durer jusqu'au 2 juin. Ce discours d'Edouard Philippe est un nouveau temps fort depuis l'annonce le 13 avril par le président Emmanuel Macron de la date de la levée partielle du confinement contre le coronavirus, en vigueur depuis le 17 mars. <br>
-                        On sait d'ores et déjà que le Premier ministre a demandé aux préfets de respecter la volonté des maires qui ne souhaitent pas rouvrir les écoles la semaine prochaine. On sait aussi qu'il donnera plus de détails sur la prime de 200 euros accordée aux plus précaires. Découvrez tout ce qu'on sait déjà des mesures qui seront mises en place à partir de lundi via le sommaire et les dernières informations sur le plan de déconfinement avec notre direct ci-dessous.</p>
+                        Le plan de sortie du confinement devrait alors être complet, au moins pour sa première phase qui devrait durer jusqu'au 2 juin. Ce discours d'Edouard Philippe est un nouveau temps fort depuis l'annonce le 13 avril par le président Emmanuel Macron de la date de la levée partielle du confinement contre le coronavirus, en vigueur depuis le 17 mars. <br></p>
                     </div>
                 </div>
                 <div id="card-index-own-style4med" class="card" style="width: 40rem;">
                     <img src="assets/news.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Blabla : Le Premier ministre Edouard Philippe a rendez-vous à 16 heures, ce jeudi 7 mai, pour présenter depuis Matignon "la préparation de l'étape du 11 mai" concoctée par son gouvernement, ainsi que la carte du déconfinement par département. Son discours est à suivre sur cette page, en direct et en vidéo.<br>
-                        Le plan de sortie du confinement devrait alors être complet, au moins pour sa première phase qui devrait durer jusqu'au 2 juin. Ce discours d'Edouard Philippe est un nouveau temps fort depuis l'annonce le 13 avril par le président Emmanuel Macron de la date de la levée partielle du confinement contre le coronavirus, en vigueur depuis le 17 mars. <br>
-                        On sait d'ores et déjà que le Premier ministre a demandé aux préfets de respecter la volonté des maires qui ne souhaitent pas rouvrir les écoles la semaine prochaine. On sait aussi qu'il donnera plus de détails sur la prime de 200 euros accordée aux plus précaires. Découvrez tout ce qu'on sait déjà des mesures qui seront mises en place à partir de lundi via le sommaire et les dernières informations sur le plan de déconfinement avec notre direct ci-dessous.</p>
+                        Le plan de sortie du confinement devrait alors être complet, au moins pour sa première phase qui devrait durer jusqu'au 2 juin. Ce discours d'Edouard Philippe est un nouveau temps fort depuis l'annonce le 13 avril par le président Emmanuel Macron de la date de la levée partielle du confinement contre le coronavirus, en vigueur depuis le 17 mars. <br></p>
                     </div>
+                </div>
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+                <div id="card-index-own-style-mobile" class="card" style="width: 55rem;">
+                    <img src="assets/news.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Blabla : Le Premier ministre Edouard Philippe a rendez-vous à 16 heures, ce jeudi 7 mai, pour présenter depuis Matignon "la préparation de l'étape du 11 mai" concoctée par son gouvernement, ainsi que la carte du déconfinement par département. Son discours est à suivre sur cette page, en direct et en vidéo.<br>
+                        Le plan de sortie du confinement devrait alors être complet, au moins pour sa première phase qui devrait durer jusqu'au 2 juin. Ce discours d'Edouard Philippe est un nouveau temps fort depuis l'annonce le 13 avril par le président Emmanuel Macron de la date de la levée partielle du confinement contre le coronavirus, en vigueur depuis le 17 mars. <br></p>
+                    </div>
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
                 </div>
                 <section id="section-news-menu-index">
                 <h6><span class="badge badge-secondary">Nouveau </span> Liste des news : </h6>
@@ -120,6 +137,49 @@
                     </ul>
                 </section>
             </section>
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+            <section id="section-news-menu-index-mobile">
+                    <section id="section-news-menu-index-mobile-flex">
+                        <h6 id="h6-mobile"><span class="badge badge-secondary">Nouveau </span> Liste des news : </h6>
+                            <div id="news-menu-index" class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action active">Blabla</a>
+                                <a href="#" class="list-group-item list-group-item-action">Blabla</a>
+                                <a href="#" class="list-group-item list-group-item-action">Blabla dates & infos !</a>
+                                <a href="#" class="list-group-item list-group-item-action">Runtrack 2 dates & infos !</a>
+                                <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Runtrack 1 dates & infos !</a>
+                            </div><br>
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <h6 id="h6-mobile"><span class="badge badge-secondary">Nouveau </span> Liste des How tos : </h6>
+                            <ul id="news-menu-index2" class="list-group">
+                                <li class="list-group-item">How to "Base de JS" (redirection meet)</li>
+                                <li class="list-group-item">How to materialize (redirection meet)</li>
+                                <li class="list-group-item">How to bootstrap proprement (redirection meet)</li>
+                                <li class="list-group-item">How to blabla (redirection meet)</li>
+                                <li class="list-group-item">How to blabla (redirection meet)</li>
+                            </ul>
+                    </section>
+                </section>
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
+                <!-- MOBILE PART -->
         </main>
         <footer>
             <section id="footerBar">
